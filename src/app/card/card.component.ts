1bb5b32c11e2201b97d97ea66775dfd71faf1213
Istvan Zilahi <istvan.zilahi98@gmail.com>
2023-06-13 13:35:11 +0200

    button hover effect, id, and new color variable have been added.

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -6,6 +6,7 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./card.component.scss'],
 })
 export class CardComponent {
+  @Input() cardId: number = -1;
   @Input() cardIconSrc: string = '';
   @Input() cardIconBg: string = '';
   @Input() cardPosted: string = '';
